545e7088156cc902d40f11710f97b6588559646d
makeworld <colecmac@protonmail.com>
2020-09-06 14:28:03 -0400

    New install method, will build flags work?

--- Formula/amfora.rb
+++ Formula/amfora.rb
@@ -15,10 +15,14 @@ class Amfora < Formula
   depends_on "go" => :build
 
   def install
+    ENV["GOPATH"] = buildpath
     ENV["GO111MODULE"] = "on"
     ENV["CGO_ENABLED"] = "0"
-    system "go", "mod", "download"
-    system "go", "build", "-o", "bin/amfora", "-ldflags=\"-s -w -X main.version=1.5.0 -X main.commit=922e7981a92cb7bf0d7b3baf1694d0fffe90d448 -X main.builtBy=official-brew-tap\""
-    bin.install "bin/amfora"
+    ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
+    (buildpath/"src/github.com/makeworld-the-better-one/amfora").install buildpath.children
+    cd "src/github.com/makeworld-the-better-one/amfora" do
+        system "go", "build", "-ldflags='-s -w -X main.version=1.5.0 -X main.commit=922e7981a92cb7bf0d7b3baf1694d0fffe90d448 -X main.builtBy=official-brew-tap'", "."
+        bin.install "amfora"
+    end
   end
 end
