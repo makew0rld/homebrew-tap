b3e7c9e1d93d77556fb1d98e1fcf14f53760f7ab
makeworld <colecmac@protonmail.com>
2020-09-15 13:37:08 -0400

    Use old code with new Makefile, specify build vars

--- Formula/amfora.rb
+++ Formula/amfora.rb
@@ -3,16 +3,19 @@
 class Amfora < Formula
   desc "A fancy terminal browser for the Gemini protocol."
   homepage "https://github.com/makeworld-the-better-one/amfora"
-  url "https://github.com/makeworld-the-better-one/amfora/archive/b2b8e30.tar.gz"
-  sha256 "d14ef4e5f13f92a209a1d2f9f9a5ccb443d8d27e06fbfa7ea880658cffe015e7"
+  url "https://github.com/makeworld-the-better-one/amfora/archive/v1.5.0.tar.gz"
+  sha256 "c8478b00155ad74712b957444b7afe025a74030907b77cca0903b6477f79843d"
   license "GPL-3.0"
   version "1.5.0"
 
   depends_on "go" => :build
 
   def install
+    # Install new Makefile
+    system "curl", "-sSL", "https://github.com/makeworld-the-better-one/amfora/raw/b2b8e30/Makefile"
     ENV["GO111MODULE"] = "on"
-    ENV["PREFIX"] = "."
+    ENV["VERSION"] = "v1.5.0"
+    ENV["COMMIT"] = "922e7981a92cb7bf0d7b3baf1694d0fffe90d448"
     ENV["BUILDER"] = "official-brew-tap"
     system "make"
     bin.install "amfora"
