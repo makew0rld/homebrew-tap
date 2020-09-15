a2f004014bb6058548a135a7cdddf9e38eac0ceb
makeworld <colecmac@protonmail.com>
2020-09-15 12:21:33 -0400

    Use new makefile

--- Formula/amfora.rb
+++ Formula/amfora.rb
@@ -3,26 +3,18 @@
 class Amfora < Formula
   desc "A fancy terminal browser for the Gemini protocol."
   homepage "https://github.com/makeworld-the-better-one/amfora"
-  url "https://github.com/makeworld-the-better-one/amfora/archive/v1.5.0.tar.gz"
+  url "https://github.com/makeworld-the-better-one/amfora.git
   sha256 "c8478b00155ad74712b957444b7afe025a74030907b77cca0903b6477f79843d"
   license "GPL-3.0"
   version "1.5.0"
 
-  head do
-    url "https://github.com/makeworld-the-better-one/amfora.git"
-  end
-
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
     ENV["GO111MODULE"] = "on"
-    ENV["CGO_ENABLED"] = "0"
-    ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
-    (buildpath/"src/github.com/makeworld-the-better-one/amfora").install buildpath.children
-    cd "src/github.com/makeworld-the-better-one/amfora" do
-        system "go", "build", "-o", bin/"amfora", "-ldflags='-s -w -X main.version=1.5.0 -X main.commit=922e7981a92cb7bf0d7b3baf1694d0fffe90d448 -X main.builtBy=official-brew-tap'", "."
-    end
+    git checkout b2b8e30
+    system "PREFIX=.", "BUILDER=official-brew-tap", "make"
+    bin.install bin/amfora
   end
 
   test do
