742444d09e5d1caea75a3bec5136111206c6fe6e
makeworld <colecmac@protonmail.com>
2020-09-15 12:25:10 -0400

    Fix syntax error

--- Formula/amfora.rb
+++ Formula/amfora.rb
@@ -3,7 +3,7 @@
 class Amfora < Formula
   desc "A fancy terminal browser for the Gemini protocol."
   homepage "https://github.com/makeworld-the-better-one/amfora"
-  url "https://github.com/makeworld-the-better-one/amfora.git
+  url "https://github.com/makeworld-the-better-one/amfora.git"
   sha256 "c8478b00155ad74712b957444b7afe025a74030907b77cca0903b6477f79843d"
   license "GPL-3.0"
   version "1.5.0"
