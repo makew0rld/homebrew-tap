3e4345548430102e8d6c9c7237ebaffd55a2b3a3
makeworld <colecmac@protonmail.com>
2020-12-24 00:02:37 -0500

    Amfora v1.7.2

--- Formula/amfora.rb
+++ Formula/amfora.rb
@@ -3,10 +3,10 @@
 class Amfora < Formula
   desc "A fancy terminal browser for the Gemini protocol."
   homepage "https://github.com/makeworld-the-better-one/amfora"
-  url "https://github.com/makeworld-the-better-one/amfora/archive/v1.7.1.tar.gz"
-  sha256 "55a2d98e438ed3d41ad6bae6b4e04348a04e28a279fdc09866a1fdd5ee91d3ec"
+  url "https://github.com/makeworld-the-better-one/amfora/archive/v1.7.2.tar.gz"
+  sha256 "3eb436a81b11b4db75dfb94e38eeaed994ce48a6a5f75ae02efd3f59cc76ec3e"
   license "GPL-3.0"
-  version "1.7.1"
+  version "1.7.2"
 
   depends_on "go" => :build
   depends_on "make" => :build
@@ -25,8 +25,8 @@ class Amfora < Formula
       system "git", "fetch", "--tags"
       system "git", "fetch", "--unshallow"
     else
-      ENV["VERSION"] = "v1.7.1"
-      ENV["COMMIT"] = "d7d7c3054e4822c418f1ca6ad119f6f05acfbea2"
+      ENV["VERSION"] = "v1.7.2"
+      ENV["COMMIT"] = "6e3e8a0584c006d2b4f082387c2361f3a6b0645f"
     end
 
     ENV["GO111MODULE"] = "on"
