6c5dfcc88c4481ce390c5c13f32b64d100caf276
makeworld <makeworld@protonmail.com>
2023-12-20 19:36:58 -0500

    didder v1.3.0

--- Formula/didder.rb
+++ Formula/didder.rb
@@ -3,10 +3,10 @@
 class Didder < Formula
   desc "An extensive, fast, and accurate command-line image dithering tool."
   homepage "https://github.com/makew0rld/didder"
-  url "https://github.com/makew0rld/didder/archive/v1.1.0.tar.gz"
-  sha256 "a850ae54125ed2aa1be54b1cc11481c00094d346861bd59014d621dc48f9aed6"
+  url "https://github.com/makew0rld/didder/archive/v1.3.0.tar.gz"
+  sha256 "6c9124eb5aa5b432065dc81d164055d94940aac3a8f14c8fb9e2f17027bd5901"
   license "GPL-3.0"
-  version "1.1.0"
+  version "1.3.0"
 
   depends_on "go" => :build
   depends_on "make" => :build
@@ -24,8 +24,8 @@ class Didder < Formula
       system "git", "fetch", "--tags"
       system "git", "fetch", "--unshallow"
     else
-      ENV["VERSION"] = "v1.1.0"
-      ENV["COMMIT"] = "9eb5bf257af601be80a7499ce4731476d226b3fc"
+      ENV["VERSION"] = "v1.3.0"
+      ENV["COMMIT"] = "409cfa6ce70d15a9c70ce3783e0d362e1cafdf4e"
     end
 
     ENV["GO111MODULE"] = "on"
