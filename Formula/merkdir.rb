868f1eebd684f443896ac2f2b7f2b760743649dc
makeworld <makeworld@protonmail.com>
2023-12-28 00:09:20 -0500

    merkdir v1.0.1

--- Formula/merkdir.rb
+++ Formula/merkdir.rb
@@ -3,10 +3,10 @@
 class Merkdir < Formula
   desc "Create Merkle trees from your directories."
   homepage "https://github.com/makew0rld/merkdir"
-  url "https://github.com/makew0rld/merkdir/archive/v1.0.0.tar.gz"
-  sha256 "382da73c97f5284489a6a9337d8ea30505808b595de9d510a3c154529c16a226"
+  url "https://github.com/makew0rld/merkdir/archive/v1.0.1.tar.gz"
+  sha256 "41ee65664ebd39bade084874c29c4a39537b7a6dac23ec467d5d19b7d9676150"
   license "GPL-3.0"
-  version "1.0.0"
+  version "1.0.1"
 
   depends_on "go" => :build
   depends_on "just" => :build
@@ -21,7 +21,7 @@ class Merkdir < Formula
       system "git", "fetch", "--unshallow"
       system "#{HOMEBREW_PREFIX}/bin/just", "builtby=homebrew-tap" 
     else
-      system "#{HOMEBREW_PREFIX}/bin/just", "builtby=homebrew-tap", "version=v1.0.0", "commit=a959087452d780b6412458df402b4b72a6739974", "date=unknown"
+      system "#{HOMEBREW_PREFIX}/bin/just", "builtby=homebrew-tap", "version=v1.0.1", "commit=37d27185a419ab63451b36d3b0514165d81d70c5", "date=unknown"
     end
     bin.install "merkdir"
   end
